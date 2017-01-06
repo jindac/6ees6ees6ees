865d3de1e4b66842fe9604ae97b637d275eecb34
Derrick Chan <dachanman91@gmail.com>
2017-01-06 10:44:26 -0800

    Implement Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
+  constructor() {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.food = 'jelly';
+    this.job = 'Keep on growing'
+  }
 };
+
+
